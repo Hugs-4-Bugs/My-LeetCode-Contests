691e0895515138c7d32dcb7695e1e186b1f7dc45
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2025-01-05 10:48:27 +0530

    Create 3264. Final Array State After K Multiplication Operations I.java

---
+++ Weekly Contest 412/3264. Final Array State After K Multiplication Operations I.java
@@ -0,0 +1,20 @@
+
+class Solution {
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        for (int i = 0; i < k; i++) {
+            int minIndex = getMinIndex(nums);
+            nums[minIndex] *= multiplier;
+        }
+        return nums;
+    }
+
+    private int getMinIndex(int[] nums) {
+        int minIndex = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[minIndex]) {
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+}
