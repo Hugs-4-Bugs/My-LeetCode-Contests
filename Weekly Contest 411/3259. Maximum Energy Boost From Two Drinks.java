6df5ccaf156c6c7e7e156b3c17b9118e1ec77c97
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2025-01-05 10:47:02 +0530

    Create 3259. Maximum Energy Boost From Two Drinks.java

---
+++ Weekly Contest 411/3259. Maximum Energy Boost From Two Drinks.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long maxEnergyBoost(int[] energyDrinkA, int[] energyDrinkB) {
+        int n = energyDrinkA.length;
+        long[] dpA = new long[n];
+        long[] dpB = new long[n];
+        
+        dpA[0] = energyDrinkA[0];
+        dpB[0] = energyDrinkB[0];
+        
+        for (int i = 1; i < n; i++) {
+            dpA[i] = Math.max(dpA[i-1] + energyDrinkA[i], dpB[i-1]);
+            dpB[i] = Math.max(dpB[i-1] + energyDrinkB[i], dpA[i-1]);
+        }
+        
+        return Math.max(dpA[n-1], dpB[n-1]);
+    }
+}
