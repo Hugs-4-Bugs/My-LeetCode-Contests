f60d960a809cd57dfbc56d340e96afa9afb14038
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2025-01-19 09:33:49 +0530

    Create Q1. Sum of Variable Length Subarrays.java

---
+++ Weekly Contest 433/Q1. Sum of Variable Length Subarrays.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int subarraySum(int[] nums) {
+        int n = nums.length;
+        int total = 0;
+
+        for (int i = 0; i < n; i++) {
+            int start = Math.max(0, i - nums[i]);
+            for (int j = start; j <= i; j++) {
+                total += nums[j];
+            }
+        }
+
+        return total;
+    }
+}
+©leetcode
